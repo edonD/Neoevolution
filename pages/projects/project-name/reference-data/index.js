df08efd04ae900c6400d8ce26a52c55259861c2d
edonD <edoonn.d@gmail.com>
2023-05-19 01:31:35 +0200

    small update-good improvement on height of datagrids

--- pages/projects/project-name/reference-data/index.js
+++ pages/projects/project-name/reference-data/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SidebarSoftware from "../../../../Components/GUI/SidebarBodies/SidebarSoftware";
 import ProfileHeader from "../../../../Components/Account/ProfileHeader/ProfileHeader";
-import styled from "styled-components";
+
 import ReferenceDataView from "../../../../Components/GUI/Reference-Data-View/ReferenceDataView";
 
 function index() {
@@ -9,13 +9,11 @@ function index() {
     <div>
       <ProfileHeader />
       {/* <Header>Lets check it!</Header> */}
-      <SidebarSoftware children={<ReferenceDataView />} />
+      <SidebarSoftware>
+        <ReferenceDataView />
+      </SidebarSoftware>
     </div>
   );
 }
 
-const Header = styled.h1`
-  margin-left: 0px;
-  margin-right: 0px;
-`;
 export default index;
