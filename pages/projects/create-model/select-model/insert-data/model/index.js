0c21eacc9d7e91837ca70d8d0e3f762a9864bb7a
edonD <edoonn.d@gmail.com>
2023-06-13 11:26:32 +0200

    fix errors and remove one file completely

--- pages/projects/create-model/select-model/insert-data/model/index.js
+++ pages/projects/create-model/select-model/insert-data/model/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import SidebarSoftware from "../../../../../../Components/GUI/SidebarBodies/SidebarSoftware";
 import ProfileHeader from "../../../../../../Components/Account/ProfileHeader/ProfileHeader";
-import ProjectModel from "../../../../../../Components/GUI/ProjectModel/ProjectModel";
 import ProjectModelReplacement from "../../../../../../Components/GUI/ProjectModel/ProjectModelReplacement";
 
 function index() {
   return (
     <div>
       <ProfileHeader />
-      {/* <Header>Lets check it!</Header> */}
       <SidebarSoftware>
         <ProjectModelReplacement />
       </SidebarSoftware>
